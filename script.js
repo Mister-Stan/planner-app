6b665e6ff40ad31f388c2cbb038f330d91c748fb
Mister Stan <lucianstan87@yahoo.com>
2023-01-27 15:03:11 +0000

    add the script file with the pseudocode

---
+++ script.js
@@ -0,0 +1,13 @@
+//create timeblocks for the business hours
+	//check localStorage for saved events and display them in their corresponding timeblocks
+	//create elements to hold the hour, events and save Button
+	//append  elements
+	//color each timeblock based on past, present and future
+	//check the current time
+	//event listener for clicking on a timeblock
+		//on click allows the user to enter an event
+	//event listener for save Button
+		//on click, save the event in localStorage
+	//check the time and update the color of the timeblocks
+//compare the current time with the time of each timeblock
+	
